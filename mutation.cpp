d817ee2eead13f02702889f180fc0e07e0292da8
Scarleth Bazaes <scarleth_bazaes@hotmail.com>
2025-06-15 18:06:20 -0400

    fixed mutation

--- mutation.cpp
+++ mutation.cpp
@@ -7,33 +7,42 @@ using namespace std;
 void swap(individuo &ind){// De toda la vida
 
     int moves_size = ind.moves.size();
-    int a = getRandomInt(0,moves_size-1);
-    int b = getRandomInt(0,moves_size-1);
-
-    int aux = ind.moves[a];
-    ind.moves[a] = ind.moves[b];
-    ind.moves[b] = aux;
-
+    for (int i = 0; i < moves_size; i++)
+    {
+        double p = getRandomProb();
+        if(p <= params.pmut){
+            int a = i;
+            int b = getRandomInt(0,moves_size-1);
+            int aux = ind.moves[a];
+            ind.moves[a] = ind.moves[b];
+            ind.moves[b] = aux;
+        }
+    }
 }
  
 void inversion(individuo &ind){// Entre dos puntos random
 
     int moves_size = ind.moves.size();
-    int a = getRandomInt(0,moves_size-1);
-    int b = getRandomInt(0,moves_size-1);
+    double p = getRandomProb();
 
-    //"a" debe ser menor o igual a b
-    if(a>b){int aux = a; a = b; b = aux;}
+    if(p <= params.pmut*moves_size){
 
-    //Invertimos sección entre a y b incluidos
-    vector<int> section;
-    for (int i = a; i < b+1; i++)
-    {
-        section.push_back(ind.moves[i]);
-    }
-    for (int i = a; i < b+1; i++)
-    {
-        ind.moves[i] = section[b-i];
+        int a = getRandomInt(0,moves_size-1);
+        int b = getRandomInt(0,moves_size-1);
+
+        //"a" debe ser menor o igual a b
+        if(a>b){int aux = a; a = b; b = aux;}
+
+        //Invertimos sección entre a y b incluidos
+        vector<int> section;
+        for (int i = a; i < b+1; i++)
+        {
+            section.push_back(ind.moves[i]);
+        }
+        for (int i = a; i < b+1; i++)
+        {
+            ind.moves[i] = section[b-i];
+        }
     }
       
 }
@@ -41,10 +50,15 @@ void inversion(individuo &ind){// Entre dos puntos random
 void intFlip(individuo &ind){// De toda la vida
 
     int moves_size = ind.moves.size();
-    int i = getRandomInt(0,moves_size-1);
 
-    ind.moves[i] = getRandomInt(1,params.n_heu);
-
+    for (int i = 0; i < moves_size; i++)
+    {
+        double p = getRandomProb();
+        if(p <= params.pmut){
+            int j = getRandomInt(0,moves_size-1);
+            ind.moves[i] = getRandomInt(1,params.n_heu);
+        }
+    }
 }
 
 //Mutar poblacion, con una probabilidad de pmut por individuo
@@ -52,19 +66,15 @@ void mutatePop(vector<individuo> &pop){
 
     for (int i = params.elite; i < params.popsize; i++)
     {
-        double p = getRandomProb();
-        if(p <= params.pmut){
-            int mut = getRandomInt(1,3);
-            if(mut == 1){
-                swap(pop[i]);
-            }
-            else if(mut == 2){
-                inversion(pop[i]);
-            }
-            else{
-                intFlip(pop[i]);
-            }
+        int mut = getRandomInt(1,3);
+        if(mut == 1){
+            swap(pop[i]);
         }
-    }
-    
+        else if(mut == 2){
+            inversion(pop[i]);
+        }
+        else{
+            intFlip(pop[i]);
+        }
+    }  
 }
--- selection.cpp
+++ selection.cpp
@@ -118,10 +118,10 @@ void generateNewPop(vector<individuo> &old_pop){
         //Realizar torneos y generar hijos en base a ganadores
         for (int i=params.elite; i<params.popsize; i+=2)
         {
-            //parent1 = tournament (old_pop[a1[i]], old_pop[a1[i+1]]);                //Pa alternar ruleta y torneo ta acá
-            //parent2 = tournament (old_pop[a2[i]], old_pop[a2[i+1]]);
-            parent1 = roulette(old_pop);            
-            parent2 = roulette(old_pop);        
+            parent1 = tournament (old_pop[a1[i]], old_pop[a1[i+1]]);                //Pa alternar ruleta y torneo ta acá
+            parent2 = tournament (old_pop[a2[i]], old_pop[a2[i+1]]);
+            //parent1 = roulette(old_pop);            
+            //parent2 = roulette(old_pop);        
             //parent2 = tournament (old_pop[a1[i+2]], old_pop[a1[i+3]]);
             children = one_point_crossover(parent1, parent2);
             new_pop[i] = children[0]; new_pop[i+1] = children[1];
